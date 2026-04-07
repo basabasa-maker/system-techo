eb48848ebc5ebacb8ad9e20747e55c2d53825b8f
平野幸大 <basabasa@hiranomiyukidainoMacBook-Pro.local>
2026-04-07 14:45:22 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v20260407-144203";
+const CACHE_VERSION = "v20260407-144458";
 const CACHE_NAME = `system-techo-${CACHE_VERSION}`;
 
 self.addEventListener("install", (event) => {
